945567f6fa843cbd3704ad12298a69f3a1f77738
Natasha Krasheninnikova <nk110905@mail.ru>
2025-06-17 21:11:11 +0700

    db.php commit

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require_once 'config/db.php'; // Подключаем БД
+
+// Получаем все записи из таблицы parking_car
+$sql = "SELECT * FROM parking_car";
+// $conn - это объект подключения к базе данных
+//->query($sql) - Это стандартный способ выполнить SQL-запрос
+$result = $conn->query($sql);
