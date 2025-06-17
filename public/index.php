0ec0ad06dbc7702248cd8300ea22c37b32add258
Natasha Krasheninnikova <nk110905@mail.ru>
2025-06-17 21:21:15 +0700

    index.php commit

--- public/index.php
+++ public/index.php
@@ -7,3 +7,20 @@ $sql = "SELECT * FROM parking_car";
 // $conn - это объект подключения к базе данных
 //->query($sql) - Это стандартный способ выполнить SQL-запрос
 $result = $conn->query($sql);
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Автостоянка</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Список автомобилей на стоянке</h1>
+</body>
+</html>
